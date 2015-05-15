7da24be88c810f376befde4b97663c7f808e938f
ianofearth <ian.anthony.bevel>
2015-05-15 10:47:14 -0700

    add validation spec to test presece of a venue name

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,5 +2,13 @@ require('spec_helper')
 
 describe(Venue) do
 	it { should have_and_belong_to_many(:bands)}
+
+	describe("validation") do
+
+		it("validates the presence of a vanue name") do
+			venue = Venue.new({:name => ""})
+			expect(venue.save()).to(eq(false))
+		end
+	end
 	
 end

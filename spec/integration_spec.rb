339b0753bf1f19dc8ddfb862771b1470b5dcb26f
ianofearth <ian.anthony.bevel>
2015-05-15 10:05:28 -0700

    add first integration spec to test add venue feature, add empty files for index.erb and venues.erb

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,3 +3,13 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe("adding a new venue", {:type => :feature}) do
+	it("allows a user to add a new venue to the list of venues") do
+		visit("/")
+		click_link("View All Venues")
+		fill_in("name", :with => "Ian's House")
+		click_button("Add Venue")
+		expect(page).to have_content("Ian's House")
+	end
+end

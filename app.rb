e8ebd3c83346c9859876938804993e0ca851e311
ianofearth <ian.anthony.bevel>
2015-05-15 10:32:33 -0700

    add content to app.rb index.erb and venues.erb to, pass add venue integration spec

---
+++ app.rb
@@ -0,0 +1,19 @@
+require('bundler/setup')
+Bundler.require(:default, :production)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+	erb(:index)
+end
+
+get("/venues") do
+	@venues = Venue.all()
+	erb(:venues)
+end
+
+post("/venues/new") do
+	venue_name = params.fetch("name")
+	@venue = Venue.new({:name => venue_name})
+	@venue.save()
+	redirect("/venues")
+end
